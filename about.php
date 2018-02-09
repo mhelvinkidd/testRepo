bc1c7b9b8168e2b3021aab0025a71502ae61243f
mhelvinkidd <mhelvinbukid28@gmail.com>
2018-02-09 10:26:58 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,18 @@
+<h1><img src="images/Gear.png"><span class="gray" style="padding-top:10px; position:absolute">SYSTEM INFO</span></h1>
+
+Version: MBA subSystem v1.	Infosys' Finacle and CARD MRI's eSystem Integrated    
+<br />
+<br />
+
+<div class="f12" style="margin-left:40px;"> 
+
+<hr class="hr"><em class="gray" style="display:inline-block; width:350px;">Development Dates:    								</em>	PHASE 1 :DEC 2011 - MARCH 2012 ; PHASE 2 :APR - JUNE 2012	
+<hr class="hr"><em class="gray" style="display:inline-block; width:350px;">Rollout Date:										</em>    		AUG 2012                					
+<hr class="hr"><em class="gray" style="display:inline-block; width:350px;"> Project Manager:									</em>  Ellen Rose Pangue 
+<hr class="hr"><em class="gray" style="display:inline-block; width:350px;"> Application Architect :								</em> Jigger Caneo
+<hr class="hr"><em class="gray" style="display:inline-block; width:350px;"> Database Architect :								</em> Jewardee Brion             					
+<hr class="hr"><em class="gray" style="display:inline-block; width:350px;"> Application Design Architect &amp; Lead Developer:	</em>  Mark Bernardo N. Paris 		           				
+<hr class="hr"><em class="gray" style="display:inline-block; width:350px;">Sub Developers:										</em>  Marlon Platta and Romeo Borja Jr. 		           
+<hr class="hr"><em class="gray" style="display:inline-block; width:350px;"> Email: 												</em>  marknpariz@gmail.com  ; mark.paris@cardbankph.com 
+
+</div>
